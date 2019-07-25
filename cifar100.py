Fixed normalization

cifar100.py
@@ -23,6 +23,7 @@ import torchvision.transforms as transforms
 import torchvision.models as tvm
 from opt import OptWBoundEignVal
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 # set seed
 np.random.seed(1226)
@@ -46,8 +47,10 @@ if not os.path.exists(root):
 trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean=[0.507, 0.487, 0.441],
                                                                         std=[0.267, 0.256, 0.276])])
 # if not exist, download cifar100 dataset
-train_set = utils_data.DataLoader(dset.CIFAR100(root=root, train=True, transform=trans, download=True), batch_size=50000)
-test_set = utils_data.DataLoader(dset.CIFAR100(root=root, train=False, transform=trans, download=True), batch_size=10000)
+train_set = utils_data.DataLoader(dset.CIFAR100(root=root, train=True, transform=trans, download=True),
+                                  batch_size=50000)
+test_set = utils_data.DataLoader(dset.CIFAR100(root=root, train=False, transform=trans, download=True),
+                                 batch_size=10000)
 
 _, (X, y) = next(enumerate(train_set))
 _, (X_test, y_test) = next(enumerate(test_set))
@@ -57,6 +60,14 @@ X_test = X_test.reshape((10000, 3, 32, 32))
 
 X, X_valid, y, y_valid = train_test_split(np.array(X), np.array(y), test_size=1/5, random_state=1226)
 
+# normalize data
+scaler = StandardScaler()
+scaler.fit(X)
+X = scaler.transform(X)
+X_valid = scaler.transform(X_valid)
+X_test = scaler.transform(X_test)
+
+# convert data-types
 X = torch.from_numpy(X)
 y = torch.from_numpy(y).long()
 X_valid = torch.from_numpy(X_valid)

cov.py
@@ -31,11 +31,13 @@ torch.manual_seed(1226)
 # Parameters
 tol = 0.001
 batch_size = 128
-mu = 0
-K = 0
+mu = 0.001
+K = 5
 
-# def mu(i):
-#    return np.max([0.0, (i-50)/1000])
+"""
+def mu(ep):
+    return np.max([0.0, (ep-49)/50000])
+"""
 
 # Load Data
 u = 'https://archive.ics.uci.edu/ml/machine-learning-databases/covtype/covtype.data.gz'
@@ -60,13 +62,14 @@ X, X_test, y, y_test = train_test_split(np.array(X), np.array(y), test_size=1/5,
 X = X.reshape((X.shape[0], 54))
 X_test = X_test.reshape((X_test.shape[0], 54))
 
+X, X_valid, y, y_valid = train_test_split(np.array(X), np.array(y), test_size=1/5, random_state=1226)
+
 # normalize data
 scaler = StandardScaler()
 scaler.fit(X)
 X = scaler.transform(X)
+X_valid = scaler.transform(X_valid)
 X_test = scaler.transform(X_test)
-
-X, X_valid, y, y_valid = train_test_split(np.array(X), np.array(y), test_size=1/5, random_state=1226)
 
 # convert data-types
 X = torch.from_numpy(X).float()
@@ -96,7 +99,7 @@ y_test = torch.from_numpy(y_test).long()
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        n = 50
+        n = 20
         self.fc1 = nn.Linear(54, n)
         self.fc2 = nn.Linear(n, n)
         self.fc3 = nn.Linear(n, 7)
@@ -117,17 +120,30 @@ alpha = lambda k: 1/(1+k)
 # Create neural network
 model = Net()
 loss = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters())
+optimizer = torch.optim.SGD(model.parameters(), lr=.5)
 scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=alpha)
 
-opt = OptWBoundEignVal(model, loss, optimizer, batch_size=batch_size, eps=-1, mu=mu, K=K, max_iter=100,
+opt = OptWBoundEignVal(model, loss, optimizer, scheduler, batch_size=batch_size, eps=-1, mu=mu, K=K, max_iter=100,
                        max_pow_iter=10000, verbose=False, header='Cov')
 
 # Train model
-opt.train(X, y, X_valid, y_valid)
+#opt.train(X, y, X_valid, y_valid)
 
-opt.test_test_set(X_test, y_test)  # test model on test set
+#opt.test_test_set(X_test, y_test)  # test model on test set
 
-test_mean = [0.0] * 54
-test_mean[0] = 0.1
-opt.test_cov_shift(X_test, y_test, test_mean=test_mean)  # test model on test set
+
+if not os.path.exists('./logs'):
+    os.mkdir('./logs')
+
+feat = 54
+old_stdout = sys.stdout  # save old output
+log_file = open("./logs/" + opt.header2 + "_cov_shift.log", "w")  # open log file
+sys.stdout = log_file  # write to log file
+for i in range(0, feat):
+    test_mean = [0.0] * feat
+    test_mean[i] = 0.1
+    print('Feature %d' % (i+1))
+    opt.test_cov_shift(X_test, y_test, test_mean=test_mean)  # test model on test set
+
+log_file.close()  # close log file
+sys.stdout = old_stdout  # reset output