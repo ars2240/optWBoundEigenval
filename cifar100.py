bb383d4c19d14c40936b147fbcfe7004a27e2139
ars2240 <ars2240@columbia.edu>
2019-08-14 11:49:30 -0500

    tweak LR

--- cifar100.py
+++ cifar100.py
@@ -51,12 +51,14 @@ test_loader = get_test_loader(batch_size=batch_size)
 
 # learning rate
 def alpha(i):
-    if i < 81:
+    if i < 60:
         return 1
-    elif i < 122:
-        return 0.1
+    elif i < 120:
+        return 0.2
+    elif i < 160:
+        return 0.2**2
     else:
-        return 0.1**2
+        return 0.2**3
 
 
 # Train Neural Network
--- usps.py
+++ usps.py
@@ -105,22 +105,18 @@ class CNN(torch.nn.Module):
         self.conv1 = nn.Conv2d(1, 8, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
         self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
 
         # linear layers
-        self.fc1 = nn.Linear(64, 64)
+        self.fc1 = nn.Linear(128, 64)
         self.fc2 = nn.Linear(64, 10)
 
     def forward(self, x):
-        # Size changes from (1, 256) to (1, 16, 16)
+        # change shape
         x = x.view(-1, 1, 16, 16)
 
-        # Computes the activation of the first convolution
-        # Size changes from (1, 16, 16) to (8, 16, 16)
+        # convolutional layers
         x = F.relu(self.conv1(x))
-
-        # Size changes from (8, 16, 16) to (8, 8, 8)
         x = self.pool(x)
 
         x = F.relu(self.conv2(x))
@@ -129,21 +125,13 @@ class CNN(torch.nn.Module):
         x = F.relu(self.conv3(x))
         x = self.pool(x)
 
-        x = F.relu(self.conv4(x))
-        x = self.pool(x)
+        # change shape
+        x = x.view(-1, 128)
 
-        # Reshape data to input to the input layer of the neural net
-        # Size changes from (16, 8, 8) to (1, 256)
-        # Recall that the -1 infers this dimension from the other given dimension
-        x = x.view(-1, 64)
-
-        # Computes the activation of the first fully connected layer
-        # Size changes from (1, 256) to (1, 64)
+        # fully connected layers
         x = F.relu(self.fc1(x))
-
-        # Computes the second fully connected layer (activation applied later)
-        # Size changes from (1, 64) to (1, 10)
         x = self.fc2(x)
+        x = F.softmax(x, dim=1)
         return x
 
 
@@ -154,7 +142,7 @@ alpha = lambda k: 1/(1+k)
 # Create neural network
 model = CNN()
 loss = torch.nn.CrossEntropyLoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
+optimizer = torch.optim.SGD(model.parameters(), lr=.5)
 scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=alpha)
 
 opt = OptWBoundEignVal(model, loss, optimizer, scheduler, batch_size=batch_size, eps=-1, mu=mu, K=K, max_iter=100,
