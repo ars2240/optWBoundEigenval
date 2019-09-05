c70edef515b7accb5612a224884c58dc0d98a740
ars2240 <ars2240@columbia.edu>
2019-09-05 10:22:47 -0500

    wide resnet 101

--- cifar10.py
+++ cifar10.py
@@ -58,7 +58,7 @@ def alpha(i):
 # Train Neural Network
 
 # Create neural network
-model = tvm.wide_resnet50_2(num_classes=10)
+model = tvm.wide_wide_resnet101_2(num_classes=10)
 loss = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0005)
 scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=alpha)
