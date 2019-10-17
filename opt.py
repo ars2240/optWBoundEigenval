7a2bfdc8e93f2c479b12e265eaa415bcf1b5476a
ars2240 <ars2240@columbia.edu>
2019-10-17 10:58:11 -0500

    bug fix

--- opt.py
+++ opt.py
@@ -19,6 +19,7 @@ from scipy.stats import norm
 from sklearn.metrics import f1_score, roc_auc_score
 import time
 import torch
+from torch.autograd import Variable
 import torch.utils.data as utils_data
 import torch.nn.functional as F
 
