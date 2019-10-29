9a61e1c4f558e5c07eeae92e5c1b289cb9f43d85
ars2240 <ars2240@columbia.edu>
2019-10-29 13:29:00 -0500

    tweak

--- chestxray.py
+++ chestxray.py
@@ -52,9 +52,7 @@ t = torch.zeros((1, 14)).to('cuda')
 n = 0
 for _, data in enumerate(train_loader):
     target = Variable(data['label']).to('cuda')
-    print(target)
-    print(t)
-    t = torch.sum(torch.cat((target, t)), dim=1)
+    t = torch.sum(torch.cat((target, t)), dim=0)
     n += len(target)
 print(t.to('cpu'))
 print(n)
