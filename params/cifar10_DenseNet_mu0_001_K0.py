e9761b80ed8c491e20dea5404dd485e31467a918
ars2240 <ars2240@columbia.edu>
2019-11-03 16:13:01 -0600

    tweak

--- params/cifar10_DenseNet_mu0_001_K0.py
+++ params/cifar10_DenseNet_mu0_001_K0.py
@@ -19,19 +19,19 @@ from densenet import DenseNet3
 
 def options():
     # create options dictionary and some parameters
-    options = {'seed': 1226, 'tol': 0.001, 'mu': 0.001, 'K': 0}
+    opt = {'seed': 1226, 'tol': 0.001, 'mu': 0.001, 'K': 0}
 
     # batch size
     batch_size = 64
-    options['batch_size'] = batch_size
+    opt['batch_size'] = batch_size
 
     # def mu(i):
     #    return np.max([0.0, (i-50)/1000])
 
     # Load the dataset
-    options['train_loader'], options['valid_loader'], options['train_loader_na'] = get_train_valid_loader(
-        batch_size=batch_size, augment=True)
-    options['test_loader'] = get_test_loader(batch_size=batch_size)
+    opt['train_loader'], opt['valid_loader'], opt['train_loader_na'] = get_train_valid_loader(batch_size=batch_size,
+                                                                                              augment=True)
+    opt['test_loader'] = get_test_loader(batch_size=batch_size)
 
     # learning rate
     def alpha(i):
@@ -43,11 +43,11 @@ def options():
             return 0.1 ** 2
 
     # Training Setup
-    options['model'] = DenseNet3(depth=40, growth_rate=12, num_classes=10)
-    options['loss'] = nn.CrossEntropyLoss()
-    options['optimizer'] = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001)
-    options['scheduler'] = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=alpha)
-    options['header'] = 'CIFAR10_DenseNet'
-    options['use_gpu'] = True
+    opt['model'] = DenseNet3(depth=40, growth_rate=12, num_classes=10)
+    opt['loss'] = nn.CrossEntropyLoss()
+    opt['optimizer'] = torch.optim.SGD(model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001)
+    opt['scheduler'] = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=alpha)
+    opt['header'] = 'CIFAR10_DenseNet'
+    opt['use_gpu'] = True
 
-    return options
+    return opt
