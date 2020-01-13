cd56f88c39f48dc3656bb2b15b59b4cb49fabfd4
ars2240 <ars2240@columbia.edu>
2020-01-13 14:04:25 -0600

    try to limit CPU threads

--- params/chestxray_mu0.py
+++ params/chestxray_mu0.py
@@ -28,9 +28,13 @@ def options():
     opt['batch_size'] = batch_size
 
     # set number of threads
+    """
     nthreads = 4
+    os.environ["OMP_NUM_THREADS"] = str(nthreads)
+    torch.set_num_threads(nthreads)
     from multiprocessing import Pool
     Pool(nthreads-1, main())
+    """
 
     # def mu(i):
     #    return np.max([0.0, (i-50)/1000])
