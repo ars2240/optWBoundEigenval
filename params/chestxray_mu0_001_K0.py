3b9c9ca02a92ef7f2fb6fd37286a3a8dc279da8b
ars2240 <ars2240@columbia.edu>
2020-04-27 11:19:45 -0500

    tweak

--- params/chestxray_mu0_001_K0.py
+++ params/chestxray_mu0_001_K0.py
@@ -41,7 +41,8 @@ def options():
 
     # normalize images
     transform = transforms.Compose([transforms.ToTensor(),
-                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+                                    transforms.Resize((256, 256)), transforms.CenterCrop((224, 224))])
 
     # Load the dataset
     train_set = ChestXray_Dataset(use='train', transform=transform)
