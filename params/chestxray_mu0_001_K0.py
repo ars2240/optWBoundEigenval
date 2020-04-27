8db10653d59d22490d99c72c5cb54eb69d1d86ec
ars2240 <ars2240@columbia.edu>
2020-04-27 11:24:35 -0500

    add parameter file

--- params/chestxray_mu0_001_K0.py
+++ params/chestxray_mu0_001_K0.py
@@ -41,8 +41,7 @@ def options():
 
     # normalize images
     transform = transforms.Compose([transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                                    transforms.Resize((256, 256)), transforms.CenterCrop((224, 224)),
-                                    transforms.ToTensor()])
+                                    transforms.Resize((256, 256)), transforms.CenterCrop((224, 224))])
 
     # Load the dataset
     train_set = ChestXray_Dataset(use='train', transform=transform)
