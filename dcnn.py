89f516ef633f24551c0630149a1f7df37554d06c
ars2240 <ars2240@columbia.edu>
2020-04-27 14:25:53 -0500

    tweak

--- dcnn.py
+++ dcnn.py
@@ -91,7 +91,7 @@ class MyAlexNet(nn.Module):
         self.features = original_model.features
         self.features.add_module('transit', nn.Sequential(nn.Conv2d(256, 1024, 3, padding=1), nn.BatchNorm2d(1024),
                                                           nn.ReLU(inplace=True), nn.MaxPool2d(2, padding=1)))
-        self.features.add_module('gpool', nn.MaxPool2d(16))
+        self.features.add_module('gpool', nn.MaxPool2d(4))
         self.classifier = nn.Linear(1024, outnum)
         
     def forward(self, x):
