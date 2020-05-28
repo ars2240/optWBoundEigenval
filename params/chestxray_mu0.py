19fa4956f151e66ea6ec7e36e850db63bbe9aa59
ars2240 <ars2240@columbia.edu>
2020-05-28 11:31:20 -0500

    tweak

--- params/chestxray_mu0.py
+++ params/chestxray_mu0.py
@@ -55,7 +55,7 @@ def options():
     transform = transforms.Compose([transforms.ToTensor(),
                                     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
                                     transforms.Resize((256, 256)), transforms.CenterCrop((224,224))])
-    test_set = CheXpert_Dataset(use='test', transform=transform)
+    test_set = CheXpert_Dataset(use='all', transform=transform)
     opt['test_loader'].append(test_set)
 
     # Create neural network
