27d257773b8ccdd0ce80e8991ca2146d65aeaa77
ars2240 <ars2240@columbia.edu>
2020-05-28 15:16:01 -0500

    tweak

--- params/chestxray_mu0.py
+++ params/chestxray_mu0.py
@@ -52,9 +52,9 @@ def options():
     test_set = ChestXray_Dataset(use='test', transform=transform)
     opt['test_loader'].append(test_set)
 
-    transform = transforms.Compose([transforms.ToTensor(),
-                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-                                    transforms.Resize((256, 256)), transforms.CenterCrop((224,224))])
+    transform = transforms.Compose([transforms.Resize((256, 256)), transforms.CenterCrop((224, 224)),
+                                    transforms.ToTensor(),
+                                    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
     test_set = CheXpert_Dataset(use='all', transform=transform)
     opt['test_loader'].append(test_set)
 
