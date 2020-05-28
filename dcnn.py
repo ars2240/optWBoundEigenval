22a270c4f6331be70fb2d709b8a8700dfbb01f1a
ars2240 <ars2240@columbia.edu>
2020-05-28 15:04:18 -0500

    tweak

--- dcnn.py
+++ dcnn.py
@@ -125,7 +125,7 @@ class CheXpert_Dataset(Dataset):
         image = Image.open(join(self.root_dir, img_name)).convert('RGB')
         labels = np.zeros(len(self.classes), dtype=np.float32)
         print(idx)
-        labels[[self.classes[x] for x in self.classes.keys() if self.label_df[[x]][idx] == 1]] = 1
+        labels[[self.classes[x] for x in self.classes.keys() if self.label_df[[x]].iloc[idx] == 1]] = 1
         # bbox = self.box_loc.loc[self.box_loc['Image Index']==img_name,['Finding Label','bbox']] \
         #        .set_index('Finding Label').to_dict()['bbox']
 
