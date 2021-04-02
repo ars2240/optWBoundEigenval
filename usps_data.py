01a6f2d125b6348506c37a638e63d01ab7cc2eec
ars2240 <ars2240@columbia.edu>
2021-04-02 15:28:23 -0500

    moved CNN to data file, adding param

--- usps_data.py
+++ usps_data.py
@@ -149,3 +149,40 @@ def get_test_loader(data_dir='./data', batch_size=1, augment=False, shuffle=Fals
 
     return data_loader
 
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        # convolutional & pooling layers
+        self.conv1 = nn.Conv2d(1, 8, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1)
+        self.conv3 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+
+        # linear layers
+        self.fc1 = nn.Linear(128, 64)
+        self.fc2 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        # change shape
+        x = x.view(-1, 1, 16, 16)
+
+        # convolutional layers
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)
+
+        x = F.relu(self.conv3(x))
+        x = self.pool(x)
+
+        # change shape
+        x = x.view(-1, 128)
+
+        # fully connected layers
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        x = F.softmax(x, dim=1)
+        return x
