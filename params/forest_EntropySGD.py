d4d01a2dd48037022278e70424cddd36b46f1217
ars2240 <ars2240@columbia.edu>
2021-05-20 11:46:36 -0500

    Add files via upload

---
+++ params/forest_EntropySGD.py
@@ -0,0 +1,45 @@
+# forest.py
+#
+# Author: Adam Sandler
+# Date: 1/19/21
+#
+# Classifies forest cover types
+#
+#
+# Dependencies:
+#   Packages: random, numpy, torch, requests, gzip, shutil, pandas
+#   Files: opt
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# import scipy.io as sio
+from forest_data import get_data, Net
+from optim import EntropySGD
+
+
+def options():
+    # create options dictionary and some parameters
+    opt = {'seed': 1226, 'tol': 0.001, 'mu': 0, 'K': 0}
+
+    # batch size
+    batch_size = 128
+    opt['batch_size'] = batch_size
+
+    # def mu(i):
+    #    return np.max([0.0, (i-50)/1000])
+
+    # Load the dataset
+    opt.update(get_data())
+
+    # Training Setup
+    opt['model'] = Net()
+    opt['loss'] = nn.CrossEntropyLoss()
+    opt['optimizer'] = EntropySGD(opt['model'].parameters())
+    # opt['scheduler'] = torch.optim.lr_scheduler.LambdaLR(options['optimizer'], lr_lambda=alpha)
+    opt['header'] = 'Forest'
+    opt['train'] = False
+    opt['pow_iter'] = False
+
+    return opt
---
+++ params/forest_lobpcg.py
@@ -0,0 +1,54 @@
+# forest.py
+#
+# Author: Adam Sandler
+# Date: 1/19/21
+#
+# Classifies forest cover types
+#
+#
+# Dependencies:
+#   Packages: random, numpy, torch, requests, gzip, shutil, pandas
+#   Files: opt
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# import scipy.io as sio
+from forest_data import get_data, Net
+
+
+def options():
+    # create options dictionary and some parameters
+    opt = {'seed': 1226, 'tol': 0.001, 'mu': 0.01, 'K': 0}
+
+    # batch size
+    batch_size = 128
+    opt['batch_size'] = batch_size
+
+    # Load the dataset
+    opt.update(get_data())
+
+    # learning rate
+    def alpha(k):
+        return np.exp(-8 * k)
+
+    def beta(k):
+        return 1/(1+k)
+
+    # Training Setup
+    opt['model'] = Net()
+    opt['loss'] = nn.CrossEntropyLoss()
+    opt['optimizer'] = torch.optim.SGD(opt['model'].parameters(), lr=0.5)
+    opt['scheduler'] = torch.optim.lr_scheduler.LambdaLR(opt['optimizer'], lr_lambda=beta)
+    opt['header'] = 'Forest_LOBPCG8'
+    opt['train'] = True
+    opt['lobpcg'] = True
+    opt['verbose'] = True
+    opt['pow_iter_alpha'] = alpha
+
+    opt['kfac_rand'] = False
+    opt['kfac_batch'] = 8
+    opt['rho_test'] = True
+
+    return opt
