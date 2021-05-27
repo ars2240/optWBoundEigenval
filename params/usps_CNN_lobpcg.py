a397737a94cd6b57ead58b4b9a437d0eb2e3448c
ars2240 <ars2240@columbia.edu>
2021-05-27 14:17:35 -0500

    teak

--- params/usps_CNN_lobpcg.py
+++ params/usps_CNN_lobpcg.py
@@ -19,7 +19,7 @@ from usps_data import get_train_valid_loader, get_test_loader, CNN
 
 def options():
     # create options dictionary and some parameters
-    opt = {'seed': 1226, 'tol': 0.001, 'mu': 0.1, 'K': 5}
+    opt = {'seed': 1226, 'tol': 0.001, 'mu': 0.01, 'K': 5}
 
     # batch size
     batch_size = 128
