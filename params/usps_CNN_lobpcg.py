38c31da817307e33bd9ee2bf187c80ba3530a723
ars2240 <ars2240@columbia.edu>
2021-05-28 12:40:41 -0500

    tweak

--- params/usps_CNN_lobpcg.py
+++ params/usps_CNN_lobpcg.py
@@ -19,7 +19,7 @@ from usps_data import get_train_valid_loader, get_test_loader, CNN
 
 def options():
     # create options dictionary and some parameters
-    opt = {'seed': 1226, 'tol': 0.001, 'mu': 0.001, 'K': 10}
+    opt = {'seed': 1226, 'tol': 0.001, 'mu': 1e-4, 'K': 10}
 
     # batch size
     batch_size = 128
@@ -48,6 +48,7 @@ def options():
     opt['lobpcg'] = True
     opt['verbose'] = True
     opt['pow_iter_alpha'] = alpha
+    opt['mem_track'] = True
 
     opt['aug_test'] = True
     opt['rho_test'] = True
