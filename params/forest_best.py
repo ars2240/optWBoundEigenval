89c48068ee0b4d42cc9e0c3dad5e526fc745be39
ars2240 <ars2240@columbia.edu>
2021-06-02 20:15:09 -0500

    tweak

---
+++ params/forest_best.py
@@ -0,0 +1,48 @@
+# forest.py
+#
+# Author: Adam Sandler
+# Date: 1/19/21
+#
+# Classifies forest cover types
+#
+#
+# Dependencies:
+#   Packages: random, numpy, torch, requests, gzip, shutil, pandas
+#   Files: opt
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# import scipy.io as sio
+from forest_data import get_data, Net
+
+
+def options():
+    # create options dictionary and some parameters
+    opt = {'seed': 1226, 'tol': 0.001, 'mu': 0.005, 'K': 1}
+
+    # batch size
+    batch_size = 128
+    opt['batch_size'] = batch_size
+
+    # Load the dataset
+    opt.update(get_data())
+
+    def beta(k):
+        return 1/(1+k)
+
+    # Training Setup
+    opt['model'] = Net()
+    opt['loss'] = nn.CrossEntropyLoss()
+    opt['optimizer'] = torch.optim.SGD(opt['model'].parameters(), lr=0.5)
+    opt['scheduler'] = torch.optim.lr_scheduler.LambdaLR(opt['optimizer'], lr_lambda=beta)
+    opt['header'] = 'Cov'
+    opt['use_gpu'] = False
+    opt['train'] = False
+    opt['lobpcg'] = False
+    opt['verbose'] = True
+    opt['rho_test'] = True
+    opt['ignore_bad_vals'] = False
+
+    return opt
