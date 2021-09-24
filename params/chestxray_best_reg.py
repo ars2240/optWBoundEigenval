15f6a78c6d30f87c99d83a75de117aa62fee1a0b
ars2240 <ars2240@columbia.edu>
2021-09-24 15:50:51 -0500

    tweak

--- params/chestxray_best_reg.py
+++ params/chestxray_best_reg.py
@@ -20,7 +20,7 @@ from dcnn import *
 
 def options():
     # create options dictionary and some parameters
-    opt = {'seed': 1226, 'tol': 0.001, 'mu': 1e-6, 'K': 0}
+    opt = {'seed': 1226, 'tol': 0.001, 'mu': 0, 'K': 0}
     enc = 'dens121'  # model type
 
     # batch size
@@ -103,7 +103,7 @@ def options():
     opt['comp_test'] = True
     opt['rho_test'] = False
     opt['other_classes'] = 0
-    # opt['fname'] = './models/m-25012018-123527.pth.tar'
+    opt['fname'] = './models/m-25012018-123527.pth.tar'
     # opt['fname'] = './models/chestxray_dens121_Adam_mu' + str(opt['mu']) + '_K' + str(opt['K']) + '_trained_model_best.pt'
 
     return opt
