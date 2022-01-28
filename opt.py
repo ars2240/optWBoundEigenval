244e93b0a3919dc6d4e72af67f547439b43e7799
ars2240 <ars2240@columbia.edu>
2022-01-28 11:31:40 -0600

    comment out grad_cam

--- opt.py
+++ opt.py
@@ -16,8 +16,8 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-from pytorch_grad_cam import GradCAM
-from pytorch_grad_cam.utils.image import show_cam_on_image
+#from pytorch_grad_cam import GradCAM
+#from pytorch_grad_cam.utils.image import show_cam_on_image
 import shutil
 import sys
 from scipy.stats import skewnorm
