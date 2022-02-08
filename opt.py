b107f60d669c702fd07e400fa4f08798c521aa08
ars2240 <ars2240@columbia.edu>
2022-02-08 13:45:36 -0600

    fix

--- opt.py
+++ opt.py
@@ -1498,8 +1498,8 @@ class OptWBoundEignVal(object):
                                 lab = list(classes[0])[mc[x]]
                                 fig, ax = plt.subplots(1, 3)
                                 fig.suptitle(lab + ', Jac={:.3f}\n'.format(jac) + tit)
-                                rgb_img = inputs[j].detach().cpu().numpy().transpose(1, 2, 0)
-                                ax[0].imshow(rgb_img, cmap='gray', vmin=0, vmax=255)
+                                rgb_img = inputs[j].detach().cpu().numpy().transpose(1, 2, 0).float()/256
+                                ax[0].imshow(rgb_img, cmap='gray', vmin=0, vmax=1)
                                 ax[0].axis('off')
                                 ax[0].set_title('XRay')
                                 if method == 'cam':
