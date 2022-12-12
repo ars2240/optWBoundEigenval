00a181861032ee6167ddca21255bd230d0c8c00d
ars2240 <ars2240@columbia.edu>
2022-12-12 11:54:51 -0600

    tweak for additional experiments

--- dcnn.py
+++ dcnn.py
@@ -22,7 +22,6 @@ from dnet import densenet121
 
 class ChestXray_Dataset(Dataset):
     """ChestXray dataset."""
-    path = '/home/hddraid/shared_data/chest_xray8/'
 
     def __init__(self, csv_labelfile='Data_Entry_2017.csv', csv_bboxfile='BBox_list_2017.csv',
                  root_dir='images/cropedimages', use='train', transform=None):
@@ -35,6 +34,7 @@ class ChestXray_Dataset(Dataset):
             transform (callable, optional): Optional transform to be applied
                 on a sample.
         """
+        path = '/home/hddraid/shared_data/chest_xray8/'
         csv_labelfile = join(path, csv_labelfile)
         csv_bboxfile = join(path, csv_bboxfile)
         root_dir = join(path, root_dir)
