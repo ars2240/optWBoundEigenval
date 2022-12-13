c4a8edb34880fa61da0eccac123d3140435790fd
ars2240 <ars2240@columbia.edu>
2022-12-13 10:43:30 -0600

    tweak for additional experiments

--- opt.py
+++ opt.py
@@ -922,9 +922,10 @@ class OptWBoundEignVal(object):
                     raise Exception('Data type not supported')
 
                 if crops:
-                    print('Crops')
+                    print(inputs.size())
                     _, _, c, h, w = inputs.size()
                     inputs = inputs.view(-1, c, h, w)
+                    print(inputs.size())
 
                 # compute loss
                 f, ops = self.comp_f(inputs, target, classes, model_classes)
