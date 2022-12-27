2a68f0314584823a2e461d99eebb5559bfd27fbd
ars2240 <ars2240@columbia.edu>
2022-12-27 17:53:53 -0600

    tweak for additional experiments

--- opt.py
+++ opt.py
@@ -925,7 +925,7 @@ class OptWBoundEignVal(object):
                     _, nc, c, h, w = inputs.size()
                     inputs = inputs.view(-1, c, h, w)
                     # target = torch.repeat_interleave(target, nc, dim=0)
-                    target = target.repeat(nc, dim=0)
+                    target = target.repeat(nc, 1)
 
                 # compute loss
                 f, ops = self.comp_f(inputs, target, classes, model_classes)
