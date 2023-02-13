9bdd201cfbf2cf634cdb22eac776bbde48ef9acc
ars2240 <ars2240@columbia.edu>
2023-02-13 10:33:43 -0600

    tweak for additional experiments

--- params/chestxray_best_reg.py
+++ params/chestxray_best_reg.py
@@ -20,7 +20,7 @@ from dcnn import *
 
 def options():
     # create options dictionary and some parameters
-    opt = {'seed': 1226, 'tol': 0.001, 'mu': 1e-5, 'K': 0}
+    opt = {'seed': 1226, 'tol': 0.001, 'mu': 1e-4, 'K': 0}
     enc = 'dens121'  # model type
 
     # batch size
@@ -106,9 +106,9 @@ def options():
     opt['model'] = model
     opt['loss'] = W_BCEWithLogitsLoss()
     # opt['loss'] = torch.nn.BCELoss(size_average=True)
-    opt['optimizer'] = torch.optim.Adam(opt['model'].parameters(), lr=1e-6, weight_decay=1e-5)
+    opt['optimizer'] = torch.optim.Adam(opt['model'].parameters(), lr=1e-7, weight_decay=1e-5)
     opt['scheduler'] = torch.optim.lr_scheduler.ReduceLROnPlateau(opt['optimizer'], patience=5)
-    opt['header'] = 'chestxray2_E-6_' + enc
+    opt['header'] = 'chestxray2_E-7_' + enc
     opt['use_gpu'] = True
     opt['pow_iter'] = True
     opt['test_func'] = 'accauc sigmoid'
