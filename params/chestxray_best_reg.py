f77245da786c28bbbb0dab965d2b83dd8f978846
ars2240 <ars2240@columbia.edu>
2023-04-12 11:21:01 -0500

    tweak for additional experiments

--- params/chestxray_best_reg.py
+++ params/chestxray_best_reg.py
@@ -108,7 +108,7 @@ def options():
     # opt['loss'] = torch.nn.BCELoss(size_average=True)
     opt['optimizer'] = torch.optim.Adam(opt['model'].parameters(), lr=1e-5, weight_decay=1e-5)
     opt['scheduler'] = torch.optim.lr_scheduler.ReduceLROnPlateau(opt['optimizer'], patience=5)
-    opt['header'] = 'chestxray2_LRE-5_ggclip10_randInit_' + enc
+    opt['header'] = 'chestxray2_LRE-5_ggclip1_randInit_' + enc
     opt['use_gpu'] = True
     opt['pow_iter'] = True
     opt['test_func'] = 'accauc sigmoid'
