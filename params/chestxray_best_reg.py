f85b57ad45db7895f4045f82c54c8b9bfb8a3e70
ars2240 <ars2240@columbia.edu>
2023-04-28 11:26:31 -0500

    tweak for additional experiments

--- params/chestxray_best_reg.py
+++ params/chestxray_best_reg.py
@@ -108,7 +108,7 @@ def options():
     # opt['loss'] = torch.nn.BCELoss(size_average=True)
     opt['optimizer'] = torch.optim.Adam(opt['model'].parameters(), lr=1e-5, weight_decay=1e-5)
     opt['scheduler'] = torch.optim.lr_scheduler.ReduceLROnPlateau(opt['optimizer'], patience=5)
-    opt['header'] = 'chestxray2_LRE-5_ggclip10_randInit_' + enc
+    opt['header'] = 'chestxray2_LRE-5_ggclip100_randInit_' + enc
     opt['use_gpu'] = True
     opt['pow_iter'] = True
     opt['test_func'] = 'accauc sigmoid'
@@ -128,7 +128,7 @@ def options():
     opt['jaccard'] = False
     opt['crops'] = True
     opt['rand_init'] = True
-    opt['gradg_clip'] = 10
+    opt['gradg_clip'] = 100
     # opt['comp_fname'] = '/home/ars411/chexnet/models/m-10012023-100132.pth.tar'
     # opt['fname'] = './models/m-25012018-123527.pth.tar'
     opt['fname'] = '/home/ars411/chexnet/models/m-10012023-100132.pth.tar'
