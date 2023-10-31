c345624bcbdd2908cd58cb5b339de0718b67ae9a
ars2240 <ars2240@columbia.edu>
2023-10-31 10:26:25 -0500

    tweak for additional experiments

--- params/chestxray_best_reg.py
+++ params/chestxray_best_reg.py
@@ -127,7 +127,7 @@ def options():
     # opt['other_classes'] = list(range(1, 7))
     opt['saliency'] = 0
     opt['jaccard'] = True
-    opt['max_img'] = 100
+    opt['max_img'] = 25
     opt['crops'] = True
     opt['rand_init'] = True
     opt['gradg_clip'] = 100
